73e84855cb7c94920f6b0deffa8244d6b711af90
VladVorobiov <vladislavovhich@gmail.com>
2020-08-19 19:46:05 +0300

    Updates

---
+++ precache-manifest.e3f6ba645e477cfb4c2d42f24d70923f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f761890698eafa54b27a5e6007a39fbb",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "bd95d2a53124ebd050ef",
+    "url": "/calculator/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bf3442b2a0a41d208c15",
+    "url": "/calculator/static/css/main.1cf7c2cf.chunk.css"
+  },
+  {
+    "revision": "bd95d2a53124ebd050ef",
+    "url": "/calculator/static/js/2.9b02a8a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculator/static/js/2.9b02a8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf3442b2a0a41d208c15",
+    "url": "/calculator/static/js/main.35f77deb.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
